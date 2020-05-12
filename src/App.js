b372a046b7f7178c2bc2e260b357182b256cbc20
gaconkzk <gaconkzk@gmail.com>
2020-05-12 08:58:29 +0700

    back to pre-configured create-react-app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
-import React, { useState } from 'react'
-import {
-  Box,
-  Grommet,
-  ResponsiveContext,
-} from 'grommet'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import AppBar from './layouts/AppBar'
-import AppSidebar from './layouts/AppSidebar'
-import MobileSidebar from './layouts/mobiles/Sidebar'
-
-const theme = {
-  global: {
-    colors: {
-      brand: '#228BE6'
-    },
-    font: {
-      family: 'Roboto',
-      size: '14px',
-      height: '20px'
-    }
-  }
-}
-
-const App = () => {
-  const [sidebar, setSidebar] = useState(false)
-
+function App() {
   return (
-    <Grommet theme={theme} full>
-      <ResponsiveContext.Consumer>
-        {size=> (
-        <Box fill>
-          <AppBar appName="Golima" sidebar={sidebar} setSidebar={setSidebar}/>
-          <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-            <Box flex align="center" justify="center">
-              app body
-            </Box>
-            {(!sidebar || size !== 'small') ? (
-              <AppSidebar show={sidebar}/>
-            ): (
-              <MobileSidebar setSidebar={setSidebar} />
-            )}
-          </Box>
-        </Box>
-        )}
-      </ResponsiveContext.Consumer>
-    </Grommet>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-export default App
+
+export default App;
