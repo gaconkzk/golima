cd8132911776e16735f7d1d5a5118dcda3ecd711
gaconkzk <gaconkzk@gmail.com>
2020-05-20 17:10:01 +0700

    fix baseurl

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import axios from 'axios'
-
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+axios.defaults.baseURL = `http://${process.env.SERVER_HOST || 'localhost'}:${process.env.SERVER_PORT || '3100' }`
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,9 +12,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-axios.defaults.baseURL = `http://${process.env.SERVER_HOST || 'localhost'}:${process.env.SERVER_PORT || '3100' }`
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
